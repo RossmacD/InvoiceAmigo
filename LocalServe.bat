ecd7833e4a2d50caf6e80bb7ad2ed0c8023ff1cc
Ross MacDonald <42966821+RossmacD@users.noreply.github.com>
2019-10-27 17:53:06 +0000

    Added Gzip, Updated Serve Batch

--- LocalServe.bat
+++ LocalServe.bat
@@ -1,2 +1,4 @@
+@echo off
 FOR /F "tokens=4 delims= " %%i in ('route print ^| find " 0.0.0.0"') do set localIp=%%i
+powershell write-host -back Red -fore Yellow WARNING WILL BREAK GOOGLE API - FOR DESIGN TESTING ONLY
 php artisan serve  --host=%localIp% 
