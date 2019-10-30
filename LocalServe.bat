ecca740c55063edb8841017f304c30f397811e5a
Ross MacDonald <42966821+RossmacD@users.noreply.github.com>
2019-10-30 16:34:49 +0000

    Update LocalServe.bat

--- LocalServe.bat
+++ LocalServe.bat
@@ -1,4 +1,5 @@
 @echo off
 FOR /F "tokens=4 delims= " %%i in ('route print ^| find " 0.0.0.0"') do set localIp=%%i
 powershell write-host -back Red -fore Yellow WARNING WILL BREAK GOOGLE API - FOR DESIGN TESTING ONLY
-php artisan serve  --host=%localIp% 
+powershell write-host -fore Cyan Ipv4: http://%localIp%:8000
+php artisan serve  --host=0.0.0.0
