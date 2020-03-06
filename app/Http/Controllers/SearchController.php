61450dedf6dae7d1998170ff07e20550357da6b5
Ross MacDonald <42966821+RossmacD@users.noreply.github.com>
2020-03-06 19:29:10 +0000

    Added AJAX Controller

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -64,4 +64,29 @@ class SearchController extends Controller
         $error = ['error' => 'No results found'];
         return response()->json($error, 200);
     }
+
+    public function searchUsers(Request $request)
+    {
+        $error = ['error' => 'Empty Query'];
+        if (!$request['keywords']) {
+            return response()->json($error, 200);
+        }
+
+        //GDPR BUG
+        $users = User::all();
+        $emals=[];
+        foreach($user as $users){
+            array_push($emails,$user->email);
+        }
+        if (!empty($users)) {
+            return response()->json(
+                [
+                    'users' => $users
+                ],
+                200
+            );
+        }
+        $error = ['error' => 'No results found'];
+        return response()->json($error, 200);
+    }
 }
